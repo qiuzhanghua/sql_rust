606916ae58d20881b7b39467f4a1f590f0d342f4
qiuzhanghua <qiuzhanghua@icloud.com>
2020-02-18 18:07:16 +0800

    use .env

--- x/src/main.rs
+++ x/src/main.rs
@@ -4,8 +4,9 @@ use sqlx::{PgPool, MySqlPool, Pool};
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
-//    let pool: PgPool = Pool::new("postgres://app:app@localhost:5432/app").await?;
-    let pool: MySqlPool = Pool::new("mysql://app:app@localhost:3306/app").await?;
+    let db_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//    let pool: PgPool = Pool::new(&db_url).await?;
+    let pool: MySqlPool = Pool::new(&db_url).await?;
     let mut pool = pool.clone();
     let recs = sqlx::query!(r#"SELECT * from people"#)
         .fetch_all(&mut pool).await.unwrap();
