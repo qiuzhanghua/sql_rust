ff04227004f534fb9cbd47a6e080670a73186ce4
Daniel <qiuzhanghua@icloud.com>
2020-04-05 21:58:59 +0800

    typo

--- x/src/main.rs
+++ x/src/main.rs
@@ -1,12 +1,13 @@
 use sqlx::types::time::PrimitiveDateTime;
 use sqlx::{self, MySqlPool, PgPool, Pool};
+use std::env;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
-    let db_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool: PgPool = Pool::new(&db_url).await?;
-    // let pool: MySqlPool = Pool::new(&db_url).await?;
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//    let pool: PgPool = Pool::new(&db_url).await?;
+    let pool: MySqlPool = Pool::new(&db_url).await?;
     let recs = sqlx::query!(r#"SELECT * from people"#)
         .fetch_all(&mut &pool)
         .await
