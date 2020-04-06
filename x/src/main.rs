56e80269440b18e7977e86c8ae8e4c96389273c7
Daniel <qiuzhanghua@icloud.com>
2020-04-06 09:58:36 +0800

    query function

--- x/src/main.rs
+++ x/src/main.rs
@@ -1,4 +1,5 @@
 // use sqlx::types::time::PrimitiveDateTime;
+use sqlx::prelude;
 use sqlx::types::chrono;
 use sqlx::{self, MySqlPool, PgPool, Pool};
 use std::env;
@@ -7,19 +8,36 @@ use std::env;
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    //   let pool: PgPool = Pool::new(&db_url).await?;
-    let pool: MySqlPool = Pool::new(&db_url).await?;
-    let recs = sqlx::query!(r#"SELECT * from people"#)
-        .fetch_all(&mut &pool)
-        .await?;
-    for rec in recs {
-        println!("{:?}", rec);
-    }
+       let pool: PgPool = Pool::new(&db_url).await?;
+//    let pool: MySqlPool = Pool::new(&db_url).await?;
+    // let recs = sqlx::query!(r#"SELECT * from people"#)
+    //     .fetch_all(&mut &pool)
+    //     .await?;
+    // for rec in recs {
+    //     println!("{:?}", rec);
+    // }
+    //
+    // let person = sqlx::query_as!(Person, r#"SELECT * from people"#)
+    //     .fetch_one(&mut &pool)
+    //     .await?;
+    // println!("{:?}", person);
 
-    let person = sqlx::query_as!(Person, r#"SELECT * from people"#)
-        .fetch_one(&mut &pool)
-        .await?;
-    println!("{:?}", person);
+    // let pool: MySqlPool = Pool::new(&db_url).await.unwrap();
+    use sqlx_core::cursor::Cursor;
+    use sqlx_core::row::Row;
+    // use sqlx_core::query::MapRow;
+
+    let mut cursor = sqlx::query(r#"SELECT * from people"#)
+        .fetch(&pool);
+     let row = cursor.next().await?.unwrap();
+    println!("{:?}", row.get::<&str, &str>("person_id"));
+
+    // let mut cursor2 = sqlx::query(r#"SELECT * from people"#)
+    //     .map(|row:MySqlRow| row.get::<String, &str>("person_id"))
+    //     .fetch_all(&pool).await.unwrap();
+    // let row = cursor2;
+    // println!("{:?}", cursor2);
+
     Ok(())
 }
 
